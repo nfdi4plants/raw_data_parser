9002a8c7f900c1ec6d3edce5c4e9e8160c666077
Johannes Werner <johannes.werner@uni-tuebingen.de>
2021-09-07 11:46:34 +0200

    implemented optional gzip and potential UMI field

--- fastq/fastq_parser.py
+++ fastq/fastq_parser.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+import gzip
 import re
 
 
-rx_pattern = r"@([a-zA-Z0-9_]+):([0-9]+):([a-zA-Z0-9]+):([0-9]+):([0-9]+):([0-9]+):([0-9]+):([ACGTN+]+) ([12]):([YN]):([0-9]+):([ACGTN])"
+rx_pattern = r"@([a-zA-Z0-9-_]+):([0-9]+):([a-zA-Z0-9]+):([0-9]+):([0-9]+):([0-9]+):([0-9]+):?([ACGTN+]*) ([12]):([YN]):([0-9]+):([ACGTN])"
 
 
 def return_module_name():
@@ -11,8 +12,12 @@ def return_module_name():
 
 
 def read_header(filename):
-    with open(filename) as f:
-        return f.readline()
+    try:
+        with gzip.open(filename, "rt") as f:
+            return f.readline()
+    except gzip.BadGzipFile:
+        with open(filename) as f:
+            return f.readline()
 
 
 def check_header(header):
@@ -20,9 +25,7 @@ def check_header(header):
 
     if not rx_match:
         return None
-    elif len(rx_match.groups()) != 12:
-        raise ValueError("ERROR: Check RegEx and header, it should return 12 fields.\n" + rx_match.groups())
-    else:
+    elif len(rx_match.groups()) == 12:
         return {
             "instrument": rx_match.group(1),
             "run_number": rx_match.group(2),
@@ -37,3 +40,19 @@ def check_header(header):
             "control_number": rx_match.group(11),
             "sample_number": rx_match.group(12)
         }
+    elif len(rx_match.groups()) == 11:
+        return {
+            "instrument": rx_match.group(1),
+            "run_number": rx_match.group(2),
+            "flowcell_ID": rx_match.group(3),
+            "lane": rx_match.group(4),
+            "tile": rx_match.group(5),
+            "xpos": rx_match.group(6),
+            "ypos": rx_match.group(7),
+            "read": rx_match.group(8),
+            "is_filtered": rx_match.group(9),
+            "control_number": rx_match.group(10),
+            "sample_number": rx_match.group(11)
+        }
+    else:
+        raise ValueError("ERROR: Check RegEx and header, it should return 12 fields.\n" + rx_match.groups())
