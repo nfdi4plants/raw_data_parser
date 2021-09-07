e312df1822400d38e1161a89775d644ed66623bc
Johannes Werner <johannes.werner@uni-tuebingen.de>
2021-09-07 11:52:06 +0200

    removed optional UMI field to avoid code redundancy

--- fastq/fastq_parser.py
+++ fastq/fastq_parser.py
@@ -4,7 +4,7 @@ import gzip
 import re
 
 
-rx_pattern = r"@([a-zA-Z0-9-_]+):([0-9]+):([a-zA-Z0-9]+):([0-9]+):([0-9]+):([0-9]+):([0-9]+):?([ACGTN+]*) ([12]):([YN]):([0-9]+):([ACGTN])"
+rx_pattern = r"@([a-zA-Z0-9-_]+):([0-9]+):([a-zA-Z0-9]+):([0-9]+):([0-9]+):([0-9]+):([0-9]+):?[ACGTN+]* ([12]):([YN]):([0-9]+):([ACGTN])"
 
 
 def return_module_name():
@@ -25,21 +25,6 @@ def check_header(header):
 
     if not rx_match:
         return None
-    elif len(rx_match.groups()) == 12:
-        return {
-            "instrument": rx_match.group(1),
-            "run_number": rx_match.group(2),
-            "flowcell_ID": rx_match.group(3),
-            "lane": rx_match.group(4),
-            "tile": rx_match.group(5),
-            "xpos": rx_match.group(6),
-            "ypos": rx_match.group(7),
-            "umi": rx_match.group(8),
-            "read": rx_match.group(9),
-            "is_filtered": rx_match.group(10),
-            "control_number": rx_match.group(11),
-            "sample_number": rx_match.group(12)
-        }
     elif len(rx_match.groups()) == 11:
         return {
             "instrument": rx_match.group(1),
